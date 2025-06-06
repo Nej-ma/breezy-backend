acf33d90b90dfd6e4ac9b106ed21ac8c14c3dd9c
Nicolas Taing <nicolas.taing9@gmail.com>
2025-06-06 09:20:16 +0200

    feat : route and controoler to get / put / post / delete Posts

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts');
 
 var app = express();
 
@@ -83,6 +84,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
