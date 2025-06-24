cbf8002b4e0169c8ee0103ba8928a75b1de7e56d
Nicolas Taing <nicolas.taing9@gmail.com>
2025-06-24 09:16:27 +0200

    docs: delete useless comments

--- microservices/auth-service/src/utils/passwordValidator.js
+++ microservices/auth-service/src/utils/passwordValidator.js
@@ -1,27 +1,22 @@
 const validatePassword = (password) => {
   const errors = [];
   
-  // Longueur minimale
   if (!password || password.length < 8) {
     errors.push('Password must be at least 8 characters long');
   }
   
-  // Au moins une majuscule
   if (!/[A-Z]/.test(password)) {
     errors.push('Password must contain at least one uppercase letter');
   }
   
-  // Au moins une minuscule
   if (!/[a-z]/.test(password)) {
     errors.push('Password must contain at least one lowercase letter');
   }
   
-  // Au moins un chiffre
   if (!/\d/.test(password)) {
     errors.push('Password must contain at least one number');
   }
   
-  // Au moins un caractère spécial
   if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
     errors.push('Password must contain at least one special character');
   }  
